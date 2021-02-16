f8814cd78469237ef3e0ec2f3abc87c0755ad423
Antoine Stouff <antoine-78280@hotmail.fr>
2021-02-16 16:01:32 +0100

    i need to put colors on my buttons when i click on the right answer

---
+++ app.js
@@ -0,0 +1,85 @@
+// TIRE SECTION
+// const tireArea = document.querySelector(".target-tire");
+// const questionTire = document.querySelector(".question-3");
+// const answersTire = document.querySelector(".answers-3");
+// const answerButtonOne = document.querySelector(".answer3.btn1");
+// const answerButtonTwo = document.querySelector(".answer3.btn2");
+// const answerButtonThree = document.querySelector(".answer3.btn3");
+// console.log(answerButtonOne);
+
+// function displayQuestionTires() {
+//     questionTire.classList.remove("not-displayed");
+//     answersTire.classList.remove("not-displayed");
+// }
+
+// function displayRedColorButtonOne(btn) {
+//     btn.classList.add("wrong-background");
+// }
+
+// function displayRedColorButtonTwo() {
+//     answerButtonTwo.classList.add("wrong-background");
+// }
+
+// displayRedColorButtonOne(answerButtonOne);
+// answerButtonOne.addEventListener("click", displayRedColorButtonOne);
+// answerButtonTwo.addEventListener("click", displayRedColorButtonTwo);
+// // answerButtonThree.addEventListener("click", displayGreenColor);
+// tireArea.addEventListener("click", displayQuestionTires);
+import {
+    questionsAnswers
+} from "./list.js";
+console.log(window.navigator);
+
+let questionPart = document.querySelector(".questions")
+let buttons
+const tireArea = document.querySelector(".target-tire");
+const batteryArea = document.querySelector(".target-battery")
+const windshieldArea = document.querySelector(".target-windshield")
+const smokeArea = document.querySelector(".target-smoke")
+
+function displayQuestion(i) {
+    let question = questionsAnswers[i].question
+    let answers = questionsAnswers[i].answers
+
+    let answersArray = [];
+    answers.forEach(answer => {
+
+        answersArray.push(answer)
+        questionPart.innerHTML = `
+        <h3 class="question">${question}</h3>
+        <button class="buttons" value="1">${answersArray[0]}</button>
+        <button class="buttons" value="2">${answersArray[1]}</button>
+        <button class="buttons" value="3">${answersArray[2]}</button>
+        <button class="buttons" value="4">${answersArray[3]}</button>
+        `
+        buttons = document.querySelectorAll(".buttons")
+        // Je veux que les boutons changent de couleurs quand on clique
+        // Chaque boutons
+        buttons.forEach(btn => {
+            // écouter le click
+            btn.addEventListener("click", colorChange);
+        })
+        // change de couleur 
+        function colorChange(evt) {
+            console.log(evt)
+            console.log(evt.target)
+            console.log(evt.target.value)
+            const element = evt.target
+            element.classList.add("wrong-background")
+        }
+
+    })
+}
+
+tireArea.addEventListener("click", function () {
+    displayQuestion(3)
+});
+batteryArea.addEventListener("click", function () {
+    displayQuestion(1)
+});
+windshieldArea.addEventListener("click", function () {
+    displayQuestion(2)
+});
+smokeArea.addEventListener("click", function () {
+    displayQuestion(0)
+});
---
+++ list.js
@@ -0,0 +1,56 @@
+export let questionsAnswers = [
+    {
+        question: "The smoke coming out of my engine is blue (yeah, it happens)What should i do ?",
+        answers: [
+            "Too much gasoline added to the engine",
+            "There's some oil added to the engine",
+            "There's some water added to the engine",
+            "Who care's? I like the blue color"
+        ],
+        answer: 2
+    },
+    {
+        question: "When i'm driving around with my Bugatti, the battery light goes on and i can't switch on the air-conditioning. What do you think is going on ?",
+        
+        answers: [
+            "The battery is down (easy)",
+            "The alternator fell of",
+            "the accessory belt has broken",
+            "The water pump fell off"
+        ],
+        answer: 3
+    },
+    {
+        question: "I have a slightly smaller impact than a 2 euro coin on my windshield at the level of my visionfield.(Hi Olivier from carglass, take a chair, it's comfortable here!) What should i do ?",
+        answers: [
+            "i'm gonna have to put some resin, i saw the add",
+            "Change the windshield, i'll only pay the deductible",
+            "Let's drive! i'm repsonsible anyway"
+        ],
+        answer: 1
+    },
+    {
+        question: "(That's an easy one) My tire on the front-left deflates, i think it's flat. So, for you, what's the procedure ?",
+        answers: [
+            "Change the two rear tires and put the old ones in the front",
+            "Fix the flat tire !",
+            "Change both front tires (that's okay, i have money)"
+        ],
+        answer: 0
+    }];
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
