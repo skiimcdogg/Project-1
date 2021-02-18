2bfe94ddb78f90cce12a62e052ce84bdf66e0b4f
Antoine Stouff <antoine-78280@hotmail.fr>
2021-02-18 17:50:07 +0100

    alldone

--- app.js
+++ app.js
@@ -31,27 +31,31 @@ import {
 console.log(window.navigator);
 
 let questionPart = document.querySelector(".questions")
-let buttons
 const tireArea = document.querySelector(".target-tire");
 const batteryArea = document.querySelector(".target-battery")
 const windshieldArea = document.querySelector(".target-windshield")
 const smokeArea = document.querySelector(".target-smoke")
 let scoreSelector = document.querySelector(".score-number")
 let livesSelector = document.querySelector(".life")
-let footerDiv
 let score = 0;
 let lives = 3;
+let buttons
+let footerDiv
 
 function displayQuestion(i) {
     let question = questionsAnswers[i].question
     let answers = questionsAnswers[i].answers
     let answerNumbers = questionsAnswers[i].answer
+    let funFact = questionsAnswers[i].funFact
     let answersArray = [];
     let answerNumbersArray = [];
+    console.log(funFact);
     let timeoutId
     answerNumbers.forEach(number => {
         answerNumbersArray.push(number);
     })
+    document.getElementById("fun-fact").innerHTML = funFact
+    document.getElementById("fun-fact").classList.add("not-displayed");
     answers.forEach(answer => {
 
         answersArray.push(answer)
@@ -64,8 +68,7 @@ function displayQuestion(i) {
         <button class="buttons" value="4">${answersArray[3]}</button>
         </div>
         `
-        // footerDiv = document.querySelector(".footer-part");
-        console.log(footerDiv);
+
         buttons = document.querySelectorAll(".buttons");
         // Je veux que les boutons changent de couleurs quand on clique
         // Chaque boutons
@@ -83,12 +86,23 @@ function displayQuestion(i) {
 
             function delayedUndisplayed() {
                 timeoutId = setTimeout(undisplayQuestionsDiv, 3000);
+
+            }
+
+            function delayeReloadPage() {
+                timeoutId2 = setTimeout(ReloadThePage, 15000);
             }
 
             function undisplayQuestionsDiv() {
                 if (eventValue == answerNumbersArray) {
                     footerDiv = document.querySelector(".footer-part");
                     footerDiv.classList.add("not-displayed");
+                }
+            }
+
+            function ReloadThePage() {
+                if (score === 4) {
+                    document.location.reload();
                 }
             }
 
@@ -99,19 +113,19 @@ function displayQuestion(i) {
                 score = score + 1;
                 scoreSelector.innerHTML = score;
                 alert("YOU deserve a point champ!");
-                // footerDiv.classList.add("not-displayed");
                 delayedUndisplayed();
+                document.getElementById("fun-fact").classList.remove("not-displayed");
                 if (score === 4) {
                     alert("CONGRATULATIONS! You're hired");
-                    document.location.reload();
+                    delayeReloadPage();
                 }
             } else {
                 eventButton.classList.add("wrong-background");
                 lives = lives - 1;
                 livesSelector.innerHTML = lives;
-                // alert("WRONG");
+                alert("WRONG");
                 if (lives === 0) {
-                    alert("GAME OVER");
+                    alert("CONGRATULATIONS! You're fired");
                     document.location.reload();
                 }
             }
--- list.js
+++ list.js
@@ -1,5 +1,4 @@
-export let questionsAnswers = [
-    {
+export let questionsAnswers = [{
         question: "The smoke coming out of my engine is blue (yeah, it happens)What should i do ?",
         answers: [
             "Too much gasoline added to the engine",
@@ -7,28 +6,31 @@ export let questionsAnswers = [
             "There's some water added to the engine",
             "Who care's? I like the blue color"
         ],
-        answer: [2]
+        answer: [2],
+        funFact: "Of course, if there is some blue smoke coming out of the engine, it means that some oil is burning with you're gasoline (due to turbo problem for example). It would be white with water and black with too much gasoline !"
     },
     {
         question: "When i'm driving around with my Bugatti, the battery light goes on and i can't switch on the air-conditioning. What do you think is going on ?",
-        
+
         answers: [
             "The battery is down (easy)",
             "The alternator fell of",
             "the accessory belt has broken",
             "The water pump fell off"
         ],
-        answer: [3]
+        answer: [3],
+        funFact: "Fun fact, that thing happened to me on my 307 (not Bugatti), if you're hear a noise (or maybe not) and you don't charge you're batterie AND the air-conditionning doesn't work anymore, it means that the accessory belt (who is attached to the alternator and the A/C) is broken."
     },
     {
-        question: "I have a slightly smaller impact than a 2 euro coin on my windshield at the level of my visionfield.(Hi Olivier from carglass, take a chair, it's comfortable here!) What should i do ?",
+        question: "I have a slightly smaller impact than a 2 euro coin on my windshield at the level of my visionfield.(Hi</br> Olivier from carglass, take a chair, it's comfortable here!) What should i do ?",
         answers: [
             "i'm gonna have to put some resin, i saw the add",
             "Change the windshield, i'll only pay the deductible",
             "Let's drive! i'm repsonsible anyway",
             "Obviously, i need to practice dodging rocks better"
         ],
-        answer: [2]
+        answer: [2],
+        funFact: "yeah you saw the add, and now you have to know that if the impact is in your field of vision, you'll have to change the entire windshield. May be Carglass will put the resin but normally they don't have the right to do so."
     },
     {
         question: "(That's an easy one) My tire on the front-left deflates, i think it's flat. So, for you, what's the procedure ?",
@@ -38,21 +40,7 @@ export let questionsAnswers = [
             "Change both front tires (that's okay, i have money)",
             "I’ll replenish my tire, it’ll give me muscles"
         ],
-        answer: [1]
-    }];
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        answer: [1],
+        funFact: "In case the front tire is flat, first of all, you'll have to change both tires because the wear pattern between the new and the old tire is different. Secondly, if you change your tires, you have to take those from the back to put them in the front, otherwise the rubber will harden and will have no more adhesion !"
+    }
+];
